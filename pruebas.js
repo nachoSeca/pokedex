2d42952e193eab5959394da8aec4165a27272035
Nacho Sánchez <secaduras87@gmail.com>
2023-12-07 10:01:10 +0100

    Modify top menu

--- pruebas.js
+++ pruebas.js
@@ -18,82 +18,91 @@ const abilities = document.getElementById("abilities");
 const habitat = document.getElementById("habitat");
 const description = document.getElementById("description");
 
+const BUTTONSEARCH = document.getElementById("buttonSearch");
+
 //CONSTATNTE PARA IMAGEN VARIA COLOR POKEMON
 /* let color2 = "";
  */
-const API_URL = "http://pokeapi.co/api/v2/pokemon/1";
-fetch(API_URL)
-  .then((response) => response.json())
-  .then((data) => {
-    /*     errorMessage.style.display = "none";
-     */ const name = data.forms[0].name;
-    pokemonName.innerHTML = name;
-    const orderData = data.id;
-    if (orderData < 10) {
-      order.innerHTML = "#00" + orderData;
-    } else if (orderData < 100) {
-      order.innerHTML = "#0" + orderData;
-    } else order.innerHTML = "#" + orderData;
-    const hpData = data.stats[0].base_stat;
-    hp.innerHTML = hpData;
-    const attackData = data.stats[1].base_stat;
-    attack.innerHTML = attackData;
-    const defenseData = data.stats[2].base_stat;
-    defense.innerHTML = defenseData;
-    const special_attackData = data.stats[3].base_stat;
-    special_attack.innerHTML = special_attackData;
-    const special_defenseData = data.stats[4].base_stat;
-    special_defense.innerHTML = special_defenseData;
-    const speedData = data.stats[5].base_stat;
-    speed.innerHTML = speedData;
-    const image = data.sprites.other["official-artwork"].front_default;
-    pokemonImage.setAttribute("src", image);
-    const color2 = data.sprites.other["official-artwork"].front_shiny;
-    image_pokemon_shiny.setAttribute("src", color2);
+BUTTONSEARCH.addEventListener("click", () => {
+  const API_URL =
+    "http://pokeapi.co/api/v2/pokemon/" +
+    document.getElementById("pokemonName").value;
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      /*     errorMessage.style.display = "none";
+       */
+      const name = data.forms[0].name;
+      pokemonName.innerHTML = name;
+      const orderData = data.id;
+      if (orderData < 10) {
+        order.innerHTML = "#00" + orderData;
+      } else if (orderData < 100) {
+        order.innerHTML = "#0" + orderData;
+      } else order.innerHTML = "#" + orderData;
+      const hpData = data.stats[0].base_stat;
+      hp.innerHTML = hpData;
+      const attackData = data.stats[1].base_stat;
+      attack.innerHTML = attackData;
+      const defenseData = data.stats[2].base_stat;
+      defense.innerHTML = defenseData;
+      const special_attackData = data.stats[3].base_stat;
+      special_attack.innerHTML = special_attackData;
+      const special_defenseData = data.stats[4].base_stat;
+      special_defense.innerHTML = special_defenseData;
+      const speedData = data.stats[5].base_stat;
+      speed.innerHTML = speedData;
+      const image = data.sprites.other["official-artwork"].front_default;
+      pokemonImage.setAttribute("src", image);
+      const color2 = data.sprites.other["official-artwork"].front_shiny;
+      image_pokemon_shiny.setAttribute("src", color2);
 
-    /*------------------------------*/
-    const typeData = data.types;
-    let aux = "";
-    typeData.forEach((types, index) => {
-      aux += types.type.name;
-      if (index !== typeData.length - 1) {
-        aux += ", ";
-      }
+      /*------------------------------*/
+      const typeData = data.types;
+      let aux = "";
+      typeData.forEach((types, index) => {
+        aux += types.type.name;
+        if (index !== typeData.length - 1) {
+          aux += ", ";
+        }
+      });
+
+      type.innerHTML = aux;
+
+      const heightData = data.height;
+      height.innerHTML = heightData;
+
+      const weightData = data.weight;
+      weight.innerHTML = weightData;
+
+      const abilitiesData = data.abilities;
+      let aux1 = "";
+      abilitiesData.forEach((ability, index) => {
+        aux1 += ability.ability.name;
+        if (index !== abilitiesData.length - 1) {
+          aux1 += ", ";
+        }
+      });
+      abilities.innerHTML = aux1;
     });
+  /*  .catch(() => {
+        errorMessage.innerHTML = "Error al obtener los datos de la API";
+        errorMessage.style.display = "block";
+      }); */
 
-    type.innerHTML = aux;
+  /* SEGUNDA COLSULTA A LA API BY ROCIO */
+  const API_URL2 =
+    "http://pokeapi.co/api/v2/pokemon-species/" +
+    document.getElementById("pokemonName").value;
+  fetch(API_URL2)
+    .then((response) => response.json())
+    .then((data) => {
+      //errorMessage.style.display = "none";
 
-    const heightData = data.height;
-    height.innerHTML = heightData;
+      const habitatData = data.habitat.name;
+      habitat.innerHTML = habitatData;
 
-    const weightData = data.weight;
-    weight.innerHTML = weightData;
-
-    const abilitiesData = data.abilities;
-    let aux1 = "";
-    abilitiesData.forEach((ability, index) => {
-      aux1 += ability.ability.name;
-      if (index !== abilitiesData.length - 1) {
-        aux1 += ", ";
-      }
+      const descriptionData = data.flavor_text_entries[8].flavor_text;
+      description.innerHTML = descriptionData;
     });
-    abilities.innerHTML = aux1;
-  });
-/*  .catch(() => {
-    errorMessage.innerHTML = "Error al obtener los datos de la API";
-    errorMessage.style.display = "block";
-  }); */
-
-/* SEGUNDA COLSULTA A LA API BY ROCIO */
-const API_URL2 = "http://pokeapi.co/api/v2/pokemon-species/1";
-fetch(API_URL2)
-  .then((response) => response.json())
-  .then((data) => {
-    //errorMessage.style.display = "none";
-
-    const habitatData = data.habitat.name;
-    habitat.innerHTML = habitatData;
-
-    const descriptionData = data.flavor_text_entries[8].flavor_text;
-    description.innerHTML = descriptionData;
-  });
+});
